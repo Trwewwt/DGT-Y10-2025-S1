04e1f8b46b94846703a3306dd45e73770ccfb753
Trwewwt <caleb.whitcombe@kingsway.school.nz>
2025-05-09 15:09:50 +1200

    Add files via upload
    Version 2 of the other one

--- Inputs and loops.py
+++ Inputs and loops.py
@@ -5,18 +5,25 @@
 
 # Ask the question and store the answer in a variable 
 # Ask the user for their name and store it 
-name = input("Kia ora, what is your name?") #stors answer \
+name = input("Kia ora, what is your name?") #stores answer
+
+print()#this adds a line break
+
+# Test that it was stored correctly
+print(f"Hello nice to meet you, {name}")
 
 # Ask the user for two numbers and then add them together 
 num1 = int(input ("what is your first number?" ))
 num2 = int(input ("what is your seccond number?" ))
 print (f"you entered your first number as {num1}")
 print (f"you entered your first number as {num2}")
-#Adding the two numbers together
 
+#Adding the two numbers together
 Sum = num1 + num2
 print (Sum)
 
-# Test that it was stored correctly
-print(name)
-# to comment code out use ctrl + /
+# Multiplying two numbers together
+multiply = num1 * num2
+
+
+# to comment code out use ctrl + / 
